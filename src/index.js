3d2c0e70dfa420c7c7bc13886c938509fb916920
sam-omar322 <abdullah.omar@lobb.sa>
2022-03-09 00:05:25 +0300

    update pizza and contact pages

--- src/index.js
+++ src/index.js
@@ -21,6 +21,37 @@ $(function() {
         $('.navbar .navbar-nav a[href="/contact.html"]').addClass('active');
     }
 
+    // show details
+    $("#details-header .details-title").on("click", function() {
+        $("#details-header .details-title").each(function() {
+            $(this).removeClass("active");
+        })
+        $(this).addClass("active");
+        switch(this.id) {
+            case "d-desc-title":
+                $("#details-content .details-content-item").each(function() {
+                    $(this).addClass("d-none");
+                    $(this).removeClass("d-block");
+                });
+                $("#details-desc").addClass("d-block");
+                break;
+            case "d-info-title":
+                $("#details-content .details-content-item").each(function() {
+                    $(this).addClass("d-none");
+                    $(this).removeClass("d-block");
+                });
+                $("#details-info").addClass("d-block");
+                break;
+            case "d-review-title":
+                $("#details-content .details-content-item").each(function() {
+                    $(this).addClass("d-none");
+                    $(this).removeClass("d-block");
+                });
+                $("#details-review").addClass("d-block");
+                break;
+        }
+    })
+
     // copyright year
     $("#year").html(new Date().getFullYear());
 });
